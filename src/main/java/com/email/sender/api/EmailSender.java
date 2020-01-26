70f02d06edcbefe810ed0919907fd3f279866add
David Zachar <dzachar@axway.com>
2020-01-26 20:58:58 +0000

    RestController

--- src/main/java/com/email/sender/api/EmailSender.java
+++ src/main/java/com/email/sender/api/EmailSender.java
@@ -1,9 +1,9 @@
 package com.email.sender.api;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class EmailSender {
 
 	@GetMapping("/test")
